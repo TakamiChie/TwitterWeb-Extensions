abf3fb1f42fd974ff701cc5e49ca8736b5f04e2b
TakamiChie@Visnu <chie@onpu-tamago.net>
2018-05-17 22:24:04 +0900

    初回コミット

---
+++ app/scripts/twitter.js
@@ -0,0 +1,73 @@
+const STARTWAIT = 5000;
+const SCENES = {
+  TIMELINE: {
+    name: "timeline",
+    time: 30000,
+  },
+  MENTION: {
+    name: "mention",
+    time: 1500,
+  },
+  NOTIFICATION: {
+    name: "notification",
+    time: 1500,
+  },
+  SEARCH: {
+    name: "search",
+    time: 30000,
+  },
+  UNDEF: {
+    name: "undefined",
+    time: 60000,
+  }
+
+}
+
+/**
+ * シーン検出を行う
+ * @param {string} url URL
+ */
+function sceneCheck(url){
+  let c = url.split(/[\/?]/);
+  console.log(c);
+  switch (c[3]) {
+    case undefined:
+    case "":
+      return SCENES.TIMELINE
+      break;
+    case "i":
+      if(c[4] == "notifications"){
+        return SCENES.NOTIFICATION;
+      }else{
+        return SCENES.UNDEF;
+      }
+      break;
+    case "mentions":
+      return SCENES.MENTION;
+      break;
+    case "search":
+      return SCENES.SEARCH;
+      break;
+    default:
+      return SCENES.UNDEF;
+      break;
+  }
+}
+
+function refresh(){
+  let scene = sceneCheck(location.href);
+  console.log(`This Scene is ${scene.name}`);
+  console.log(`Wait Time of ${scene.time}`);
+  if(document.readyState != "interactive"){
+    if(document.body.scrollTop + window.scrollY == 0){
+      if(document.querySelectorAll(".js-new-tweets-bar").length > 0){
+        document.querySelector(".js-new-tweets-bar").click();
+      }
+    }
+  }
+  window.setTimeout(refresh, scene.time);
+}
+
+window.setTimeout(refresh, STARTWAIT);
+
+

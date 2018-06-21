fa0b6a1731da7d18378e893b54056a0a58e5c195
TakamiChie@Visnu <chie@onpu-tamago.net>
2018-06-21 14:17:57 +0900

    オプションページ実装中(正常動作せず)

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,4 +1,50 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
 
-console.log(`'Allo 'Allo! Options`)
+function rangeChanged(e){
+  saveOption(e.target);
+}
+
+function rangeInputed(e){
+  updateLabel(e.target);
+}
+
+function updateLabel(obj){
+  document.getElementById(obj.id + "_number").textContent = 
+    obj.value / 1000;
+}
+
+function saveOption(obj){
+  let setName = "reload_" + obj.id;
+  chrome.storage.sync.set({
+    setName: obj.value
+  }, () => {
+  });
+}
+
+function init(){
+  let tl = document.getElementById("timeline");
+  let mention = document.getElementById("mention");
+  let search = document.getElementById("search");
+  let a = [tl, mention, search];
+  a.forEach((value) => {
+    value.addEventListener("input", rangeInputed);
+    value.addEventListener("change", rangeChanged);
+    updateLabel(value);
+  });
+
+  // 設定読み込み
+  chrome.storage.sync.get({
+    "reload_timeline": 30000,
+    "reload_mention": 1000,
+    "reload_search": 20000
+  }, (items) => {
+    tl.value = items.reload_timeline;
+    mention.value = items.reload_mention;
+    search.value = items.reload_search;
+    updateLabel(tl);
+    updateLabel(mention);
+    updateLabel(search);
+  });
+}
+document.addEventListener('DOMContentLoaded', init);
